ab533ddd20e36e602ee4de439b6b59c1ad44b84e
amdevper <68313421+amdevper@users.noreply.github.com>
2021-08-22 21:13:52 +0700

    pug install

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const app = express()
+const path = require("path")
+const router = express.Router()
 const PORT = process.env.PORT || 8080
 
+/* 
 app.get('/',(req,res)=>{
     res.json({result:"OK",data:[1,2,3,4,5]})
 })
-
+*/
+/* 
 app.get('/hello',(req,res)=>{
     res.json({result:"OKNG",data:[2,11,322,433,52]})
 })
+*/
+app.set("view engine","pug")
 app.listen(PORT,()=>{
     console.log(`App running at port ${PORT}`)
 })
